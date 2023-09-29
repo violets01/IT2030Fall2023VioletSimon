d63e7dc3cd4abd1449eca78251cedaca261a6978
violets01 <89762777+violets01@users.noreply.github.com>
2023-09-28 20:44:57 -0400

    Add files via upload

---
+++ Week 5 Lab/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using VioletsLoops.Models;
+    public class ContactController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactModel model)
+        {
+        
+            return View(model);
+        }
+    }
---
+++ Week 5 Lab/Models/ContactModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VioletsLoops.Models
+{
+    public class ContactModel
+    {
+        [Required(ErrorMessage = "Please enter a first name.")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a last name.")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid address.")]
+        public string? Address { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid phone number.")]
+        [Range(1000000000, 9999999999, ErrorMessage ="Phone number must be 10 digets long")]
+        public string? Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid email address.")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Please tell us how we can help you.")]
+        public string? Message { get; set; }
+    }
+}
